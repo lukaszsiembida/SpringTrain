082e3246a6a354680380cd759dd95c0d3e6d3baf
lukasz <lukaszsiembida@wp.pl>
2020-10-25 15:27:41 +0100

    crone

--- src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
+++ src/main/java/pl/sda/springtrainingjavalub22/SpringTrainingJavalub22Application.java
@@ -3,8 +3,10 @@ package pl.sda.springtrainingjavalub22;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import pl.sda.springtrainingjavalub22.config.CompanyInfo;
 
+@EnableScheduling // pozwala tworzyć funkcje które odpalają się po określonym interwale czasowym
 @SpringBootApplication
 @EnableConfigurationProperties(value = {CompanyInfo.class}) // dołączenie konfiguracji z application.properties , dodanie beana CompanyInfo
 public class SpringTrainingJavalub22Application {
---
+++ src/main/java/pl/sda/springtrainingjavalub22/domain/car/CarTask.java
@@ -0,0 +1,38 @@
+package pl.sda.springtrainingjavalub22.domain.car;
+
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class CarTask {
+
+    private CarService carService;
+
+
+    // fixedDelay  - od zakończenia poprzedniego
+    // fixedRate - funkcja odpala się co określony czas
+    // https://crontab.cronhub.io/   cron generator
+    @Scheduled(cron="0 21 15 * * ?")  // odpala się o 15:21 każdego dnia
+    public void checkInsurance(){
+        System.out.println("\n\n\n\n CRON FUNCTION \n\n\n\n");
+        List<Car> all =carService.getAll();
+        all.stream().filter((this::isInsuranceCloseToEnd))
+                .forEach(car -> System.out.println(
+                        String.format("Dla auta o VIN %s ubezpieczenie kończy się dnia %s",
+                                car.getVin(), car.getInsuredTo().toString())));
+    }
+
+    private boolean isInsuranceCloseToEnd(Car car){
+        LocalDate now = LocalDate.now();
+        return car.getInsuredTo().isBefore(now) ||
+                ChronoUnit.DAYS.between(now, car.getInsuredTo()) < 10;
+    }
+
+
+}
